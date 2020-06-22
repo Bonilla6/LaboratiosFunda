573578e80a029720e48e6878f825ae82b410a02a
Bonilla6 <adabonilla114@gmail.com>
2020-06-22 17:44:09 -0600

    lab6

--- Lab6/ejercicio5.cpp
+++ Lab6/ejercicio5.cpp
@@ -5,20 +5,19 @@ using namespace std;
 int main()
 {
     int nimpares[100], n, i;
-    nimpares[100] = {1};
+    nimpares[0] = 1;
+
     n = 1;
+
     for (int i = 1; i < 100; i++)
     {
-        n = n + 2;
+        n = n +2;
         nimpares[i] = n;
     }
     cout << "Esta es la lista de los primeros 100 numeros impares" << endl;
 
-    for (int i=99; i > 1; i--)
+    for ( i=99; i >=0 ; i--)
     {
        cout << nimpares[i] << endl;
-       cout << 3 << endl;
-       cout << 1 << endl;
     }
-    
-}
+}    
